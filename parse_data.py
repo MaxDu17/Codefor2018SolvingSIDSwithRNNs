6aca58cbdfbabb849d7b35fd9221fc74fa9df266
Max Du <maxdubusiness@gmail.com>
2018-02-19 18:12:40 -0500

    binning

--- parse_data.py
+++ parse_data.py
@@ -29,20 +29,55 @@ def load_wav_file(name):
     #data = (data+128)/255
     return data
 
-file_name = Source.Native.INHALE_DIR + "1.wav"
-test = load_wav_file(file_name)
-file_name = Source.Native.EXHALE_DIR + "5.wav"
-test1 = load_wav_file(file_name)
-output = np.abs(np.fft.fft(test))
-output = output[1:CHUNK]
-output = (output - np.min(output))/(np.max(output)-np.min(output))
-frequency_div = np.linspace(1,CHUNK/2, CHUNK-1)
+
+def split_file(data):
+
+    time_split = list()
+    for i in range(16):
+        current_block_start = i*512
+        current_block_end = current_block_start + 512
+        time_split.append(data[current_block_start:current_block_end])
+
+    return time_split
 
 
+def load_fourier(data):
+
+    fourier_output = np.abs(np.fft.fft(data))
+    fourier_output = fourier_output[1:int(CHUNK/16)]#truncates according to nyquist limit
+    fourier_output_norm = (fourier_output - np.min(fourier_output)) / (np.max(fourier_output) - np.min(fourier_output))
+    frequency_division = np.linspace(1,CHUNK/2, int((CHUNK/16)-1))
+    return fourier_output_norm, frequency_division
+
+def bin(output):
+    output_bins = [None]*43
+
+    for i in range(42):
+        first = i*6
+        last = first+6
+        output_bins[i] = np.mean(output[first:last])
+    output_bins[42] = np.mean(output[42*6:])
+    return output_bins
+
+def prepare_data(name):
+    data_list = list()
+    data = load_wav_file(name)
+    time_split = split_file(data)
+    for time_slice in time_split:
+        output, frq_div = load_fourier(time_slice)
+        output_bins = bin(output)
+        data_list.append(output_bins)
+    return data_list
+
+
+file_name = Source.Native.EXHALE_DIR + "1.wav"
+data_list = prepare_data(file_name)
+print(len(data_list))
+'''
 output1 = np.abs(np.fft.fft(test1))
 output1 = (output1 - np.min(output1))/(np.max(output1)-np.min(output1))
 output1 = output1[1:CHUNK]
 
 
-plt.plot(frequency_div, output,output1)
-plt.show()
+plt.plot(frq_div, output)
+plt.show()'''

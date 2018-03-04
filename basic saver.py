1aea6951c8ba6d558437a9d5676057a0546b2cdb
Max Du <maxdubusiness@gmail.com>
2018-03-03 20:21:17 -0500

    b

--- basic saver.py
+++ basic saver.py
@@ -4,6 +4,9 @@ a = tf.Variable(3)
 b = tf.placeholder(tf.int32, name = "inplace")
 
 final = tf.add(a, b)
-
-
-
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    result = sess.run(final, feed_dict = {b:12})
+    saver.save(sess, "scratch/test",global_step = 1)
+print(result)

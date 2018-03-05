3e3b9ee6861814f3d0d925ab4cf3853351e52b88
Max Du <maxdubusiness@gmail.com>
2018-03-04 22:35:07 -0500

    whee

--- run_stream_net.py
+++ run_stream_net.py
@@ -16,6 +16,8 @@ JUMPTIME = 2
 FRAMERATE = 4096
 CHUNK = 8192
 OFFSET = 512
+TIMEOUT = 2048
+ALPHALEVEL = 0.995
 prediction_dictionary = {0:"inhale", 1:"exhale", 2:"unknown"}
 def test_implementation():
     file = "dataTEST/inhale/1.wav"
@@ -32,6 +34,7 @@ def test_from_file():
     wav_file = wave.open(file, 'r')
     x = -1
     last_x = -1
+    time_diff = 99999
     for i in range(2399):
         data = wav_file.readframes(CHUNK)
         wav_file.setpos((i+1)*OFFSET)
@@ -41,17 +44,19 @@ def test_from_file():
         prediction = RunGraph.make_prediction(parsed_data)
         writer.writerow(prediction[0])
         x = np.argmax(prediction[0])
+        time = i*OFFSET
         if x != 2:
-            if prediction[0][x] > 0.95:
+            if prediction[0][x] > ALPHALEVEL:
                 last_x = x
+                time_last = i*OFFSET
         elif x == 2:
-            if last_x !=2:
-                carrier = [prediction_dictionary[last_x], i / 8]
-                writer_log.writerow(carrier)
+            if last_x !=2 and last_x != -1:
+                if time-time_last>TIMEOUT:
+                    carrier = [prediction_dictionary[last_x], i / 8]
+                    writer_log.writerow(carrier)
+                    last_x = x
+            else:
                 last_x = x
-
-
-
+                pass
     wav_file.close()
 
-test_from_file()

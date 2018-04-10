7a75b9d93481d16290b6f0d524329f33ecc1ba5a
Max Du <maxdubusiness@gmail.com>
2018-04-10 18:26:16 -0400

    yep

--- noisereduction.py
+++ noisereduction.py
@@ -1,45 +1,26 @@
 import numpy as np
 import wave
 import struct
+import scipy.io.wavfile
 
 
-
-CHUNK = 44100
+CHUNK = 4096
 RECORDTIME = 10
 
 
+rate, data_filled = scipy.io.wavfile.read('noisereduction/blank.wav')
+rate_, data_empty = scipy.io.wavfile.read('noisereduction/filled.wav')
 
-def load_wav_file(name):
-    # print(name)
-    wav_file = wave.open(name, 'r')
-    data = wav_file.readframes(RECORDTIME * CHUNK)
-    print(len(data))
-    wav_file.close()
-    data = struct.unpack('{n}h'.format(n=RECORDTIME * CHUNK), data)
-    data = np.array(data)
-    return data
-
-
-#data_blank = load_wav_file("noisereduction/blank.wav")
-data_filled = load_wav_file("noisereduction/filledyet44100.wav")
-#fourier_blank = np.fft.fft(data_blank)
-fourier_filled = np.fft.fft(data_filled)
-
-test = fourier_filled
-#transformed = np.fft.ifft(test)
-transformed = np.fft.ifft(np.fft.fft(data_filled))
-transformed = transformed.astype('uint8')
-print(len(transformed))
-print(transformed)
+blank = np.fft.fft(data_empty)
+full = np.fft.fft(data_filled)
+carrier = full
+transformed = np.fft.ifft(np.fft.fft(carrier))
+#transformed = transformed.astype('uint8')
+transformed = [int(np.real(trm)) for trm in transformed]
+transformed = np.array(transformed)
 
 
-wav_file = wave.open("noisereduction/filledyet44100.wav", 'r')
-wf = wave.open("noisereduction/subtraction.wav", 'wb')
+transformed = transformed.astype("int16")
 
-wf.setparams(wav_file.getparams())
 
-for sample in transformed:
-    value = struct.pack('h', sample)
-    wf.writeframesraw(value)
-
-wf.close()
+scipy.io.wavfile.write('noisereduction/cancel.wav', rate, transformed[:])

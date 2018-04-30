ea02beaa741ce9a2d97095acfe1fc892e2b4210f
Max Du <maxdubusiness@gmail.com>
2018-04-30 00:53:41 -0400

    asdf

--- autoencoderplotter.py
+++ autoencoderplotter.py
@@ -6,15 +6,16 @@ from make_sets import Source as SS
 
 source_dir = SS()
 graph = WG()
-BINS = 10
+BINS = 50
 
-name = "sen_data/inhale/" + "3" +".wav"
+name = "sen_data/inhale/" + "154" +".wav"
 
 matrix = graph.make_matrix_from_name(name)
-print(len(matrix))
-print(matrix[0])
+aa = np.array(matrix)
+carrier = np.reshape(aa, [16,50])
+
 numbers = np.linspace(0,15,16)
-new_matrix =np.reshape(matrix, [50,16])
+new_matrix =np.transpose(carrier)
 plt.figure(num='INHALE')
 for i in range(BINS):
     plt.plot(numbers, new_matrix[i], label = str(i))
@@ -22,21 +23,29 @@ plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=10, mode="expand", borderaxespad=0.)
 
 
-name = "sen_data/unknown/" + "25" +".wav"
+name = "sen_data/unknown/" + "120" +".wav"
+
 matrix = graph.make_matrix_from_name(name)
+aa = np.array(matrix)
+carrier = np.reshape(aa, [16,50])
+
 numbers = np.linspace(0,15,16)
-new_matrix =np.reshape(matrix, [50,16])
+new_matrix =np.transpose(carrier)
 plt.figure(num='NOISE')
 for i in range(BINS):
     plt.plot(numbers, new_matrix[i], label = str(i))
 plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
            ncol=10, mode="expand", borderaxespad=0.)
 
-name = "sen_data/exhale/" + "5" +".wav"
+name = "sen_data/exhale/" + "39" +".wav"
+
 
 matrix = graph.make_matrix_from_name(name)
+aa = np.array(matrix)
+carrier = np.reshape(aa, [16,50])
+
 numbers = np.linspace(0,15,16)
-new_matrix =np.reshape(matrix, [50,16])
+new_matrix =np.transpose(carrier)
 plt.figure(num='EXHALE')
 for i in range(BINS):
     plt.plot(numbers, new_matrix[i], label = str(i))
